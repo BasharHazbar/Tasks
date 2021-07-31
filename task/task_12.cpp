92d935c49657c3eb2c3122de7a1c49dd3949fb6d
bsh1999 <hbashark99@gmail.com>
2021-07-31 13:47:16 +0700

    the twelfth task

---
+++ task/task_12.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <array>
+#include <string>
+using namespace std;
+
+void dis_arr(int arr[],int n,int i){
+    if (i == n){
+        return;
+    }
+
+    else{
+      cout << arr[i] << " ";
+      dis_arr(arr,n,i + 1);
+    }
+
+}
+
+int max_num(int arr[],int n){
+    int mx = arr[0];
+    if (n < 0){
+      return 0;
+    }
+    else {
+        if (arr[n] > mx){
+            mx = arr[n];
+        }
+        max_num(arr,n - 1);
+    }
+    return mx;
+
+}
+int min_num(int arr[],int n){
+    int mn = arr[0];
+    if (n < 0){
+      return 0;
+    }
+    else {
+        if (arr[n] < mn){
+            mn = arr[n];
+        }
+        max_num(arr,n - 1);
+    }
+    return mn;
+
+}
+
+int main(){
+    int n,i;
+    cout << "enter numbers of elements: ";
+    cin >> n;
+    int arr[n];
+    for (i = 0; i < n; i++){
+        cout << "inter the number_" << i + 1 << ": ";
+        cin >> arr[i];
+    }
+    dis_arr(arr,n,i = 0);
+    cout << endl;
+
+    cout << "the max number is: " << max_num(arr,n - 1) << endl;
+    cout << "the min number is: " << min_num(arr,n - 1);
+    return 0;
+}
