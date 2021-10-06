b5a1080a62773dab1b6bfccb7ecb52c1ed8effee
bsh1999 <hbashark99@gmail.com>
2021-10-06 21:45:13 +0700

    task_34

---
+++ task/task_34.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+using namespace std;
+
+
+int main(){
+    int n = 5,i,j,duplicate = 0,total = 0;
+    int arr[n];
+    int arr_2[n];
+    int arr_3[n];
+
+    for (i = 0; i < n; i++){
+        cout << "element_" << i + 1 << ": ";
+        cin >> arr[i];
+    }
+
+    for (i = 0; i < n; i++){
+        arr_2[i] = arr[i];
+    }
+
+
+    cout << "numbers unique is: ";
+    for (i = 0; i < n; i++){
+        int temp = 0;
+        for(j = 0; j < n; j++){
+                if(i != j){
+                    if(arr[i] == arr[j]){
+                       temp++;
+                       break;
+                    }
+                }
+        }
+        if(temp == 0){
+            cout << arr[i] << "  ";
+        }
+    }
+
+    cout << "total: ";
+    for (i = 0; i < n; i++){
+        int temp = 0;
+        for(j = 0; j < n; j++){
+                if(i != j){
+                    if(arr[i] == arr[j]){
+                       temp++;
+                    }
+                }
+        }
+        cout << temp;
+
+    }
+
+
+
+
+
+
+
+
+return 0;
+}
